60c5b6d54edf4aaefd8d3c788e77c3677a0dc53e
jsillo <Jolomi@example.com>
2023-12-09 16:08:30 +0000

    setup.py read requirements mod3

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
 
-# Read requirements.txt and use its contents as the install requirements
-# with open('requirements.txt', 'r', 'utf-8-sig') as f:
-#     requirements = f.read().splitlines()
-
-with open('requirements.txt', encoding='utf-8-sig') as f:
-    requirements = f.read().splitlines()
+requirements = [
+    'blinker==1.7.0',
+    'click==8.1.7',
+    'colorama==0.4.6',
+    'Flask==3.0.0',
+    'Flask-SQLAlchemy==3.1.1',
+    'greenlet==3.0.1',
+    'itsdangerous==2.1.2',
+    'Jinja2==3.1.2',
+    'MarkupSafe==2.1.3',
+    'SQLAlchemy==2.0.23',
+    'typing_extensions==4.8.0',
+    'Werkzeug==3.0.1',
+    'gunicorn'
+]
 
 setup(
     name='Volun2k9App',

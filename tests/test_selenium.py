1708bdd00c1426650ef3d36666c11371df38bfbe
jsillo <Jolomi@example.com>
2023-12-11 05:32:33 +0000

    added selenium9

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import unittest
 
+
 class FlaskAppTest(unittest.TestCase):
 
     def setUp(self):

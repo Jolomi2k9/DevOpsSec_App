b7314d2f5f01b0b3a1b19951eafceca55bdefc44
jsillo <Jolomi@example.com>
2023-12-11 03:42:44 +0000

    added selenium6

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import unittest
 
 class ExampleComTestCase(unittest.TestCase):
@@ -6,7 +7,9 @@ class ExampleComTestCase(unittest.TestCase):
     def setUp(self):
         # Initialize the Chrome WebDriver instance
         # Make sure the 'chromedriver' executable is in your PATH or provide the path to the executable.
-        self.driver = webdriver.Chrome()
+        options = Options()
+        options.headless = True
+        self.driver = webdriver.Chrome(options=options)
 
     def test_title(self):
         # Replace 'Example Domain' with the actual expected title of your Flask application's index page.

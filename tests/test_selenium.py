e3164aaaeb5207d8a2a83640f690a09cb1ae95f1
jsillo <Jolomi@example.com>
2023-12-11 04:45:35 +0000

    added selenium7

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -2,23 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import unittest
 
-class ExampleComTestCase(unittest.TestCase):
+class FlaskAppTest(unittest.TestCase):
 
     def setUp(self):
-        # Initialize the Chrome WebDriver instance
-        # Make sure the 'chromedriver' executable is in your PATH or provide the path to the executable.
+
         options = Options()
         options.headless = True
         self.driver = webdriver.Chrome(options=options)
 
-    def test_title(self):
-        # Replace 'Example Domain' with the actual expected title of your Flask application's index page.
-        expected_title = "Your Expected Title Here"
+    def test_title(self):        
+        expected_title = "Volunteer app"
         self.driver.get("http://127.0.0.1:5000")
         self.assertIn(expected_title, self.driver.title)
 
     def tearDown(self):
-        # Quit the driver after the test suite is completed.
+        # stop the driver after the test suite is completed.
         self.driver.quit()
 
 if __name__ == "__main__":

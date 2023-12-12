bd58202cc4bba264acca01617edcda845194abe3
jsillo <Jolomi@example.com>
2023-12-12 18:41:33 +0000

    retry selenium testing5

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,5 +1,7 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager

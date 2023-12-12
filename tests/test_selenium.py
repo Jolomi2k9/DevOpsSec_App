3ec25b9ca99d16d61f2bf689f972cc787f7c9877
jsillo <Jolomi@example.com>
2023-12-12 18:19:16 +0000

    retry selenium testing3

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -2,16 +2,21 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
 class TestApp(unittest.TestCase):
 
     def setUp(self):
-        # Initialize Chrome WebDriver using ChromeDriverManager
+        # Set up the Chrome WebDriver with the desired options
         options = Options()
-        options.headless = True  # Run in headless mode
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")  # Crucial for Jenkins or Docker environments
+        options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+        options.add_argument("--disable-gpu")  # GPU not used in headless mode
+        options.add_argument("--disable-extensions")  # Disable any browser extensions during the test
+        options.add_argument("--disable-infobars")  # Prevent infobars from appearing
+        
+        # Initialize Chrome WebDriver using ChromeDriverManager
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
     def test_title(self):
@@ -28,4 +33,5 @@ class TestApp(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
 
